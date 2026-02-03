3791a2d0a95bc4c8bc65a70ddb3ea36bc1dd3af9
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2026-02-03 20:28:45 +0100

    Use FetchContent with globs and create separate libs

---
+++ cmake/Sources.cmake
@@ -0,0 +1,162 @@
+if (NOT DEFINED foobar_sdk_SOURCE_DIR)
+    message(FATAL_ERROR "foobar_sdk_SOURCE_DIR is not defined; FetchContent must be configured before including SourcesWindows.cmake")
+endif ()
+set(FLAGS)
+if (WIN32)
+    set(WINVER 0x0601 CACHE STRING "Target Windows version")
+    set(FLAGS
+            _UNICODE
+            UNICODE
+            STRICT
+            WINVER=${WINVER}
+            _WIN32_WINNT=${WINVER}
+    )
+endif ()
+set(_foosdk_glob_root "${foobar_sdk_SOURCE_DIR}")
+
+function(_foosdk_glob out_var)
+    file(GLOB _globbed CONFIGURE_DEPENDS LIST_DIRECTORIES FALSE RELATIVE "${_foosdk_glob_root}" ${ARGN})
+    list(SORT _globbed)
+    list(TRANSFORM _globbed PREPEND "${_foosdk_glob_root}/")
+    set(${out_var} "${_globbed}" PARENT_SCOPE)
+endfunction()
+
+_foosdk_glob(PFC_SOURCES
+        "${_foosdk_glob_root}/pfc/*.c"
+        "${_foosdk_glob_root}/pfc/*.cpp"
+)
+if (APPLE)
+    _foosdk_glob(PFC_SOURCES_MAC
+            "${_foosdk_glob_root}/pfc/*.m"
+            "${_foosdk_glob_root}/pfc/*.mm"
+    )
+    list(APPEND PFC_SOURCES ${PFC_SOURCES_MAC})
+endif ()
+
+add_library(pfc STATIC ${PFC_SOURCES})
+target_include_directories(pfc PUBLIC
+        "$<BUILD_INTERFACE:${_foosdk_glob_root}>"
+        "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>"
+)
+target_compile_definitions(pfc PUBLIC ${FLAGS})
+
+_foosdk_glob(SDK_SOURCES
+        "${_foosdk_glob_root}/foobar2000/SDK/*.c"
+        "${_foosdk_glob_root}/foobar2000/SDK/*.cpp"
+)
+if (APPLE)
+    _foosdk_glob(SDK_SOURCES_MAC
+            "${_foosdk_glob_root}/foobar2000/SDK/*.m"
+            "${_foosdk_glob_root}/foobar2000/SDK/*.mm"
+    )
+    list(APPEND SDK_SOURCES ${SDK_SOURCES_MAC})
+endif ()
+add_library(foosdk STATIC ${SDK_SOURCES})
+target_include_directories(foosdk PUBLIC
+        "$<BUILD_INTERFACE:${_foosdk_glob_root}/foobar2000>"
+        "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000>"
+)
+target_link_libraries(foosdk PUBLIC pfc)
+
+_foosdk_glob(SDK_HELPERS_SOURCES
+        "${_foosdk_glob_root}/foobar2000/helpers/*.c"
+        "${_foosdk_glob_root}/foobar2000/helpers/*.cpp"
+)
+add_library(foosdk_helpers STATIC ${SDK_HELPERS_SOURCES})
+target_include_directories(foosdk_helpers PUBLIC
+        "$<BUILD_INTERFACE:${_foosdk_glob_root}/foobar2000/helpers>"
+        "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000/helpers>"
+)
+target_link_libraries(foosdk_helpers PUBLIC pfc foosdk)
+if (WIN32)
+    target_link_libraries(foosdk_helpers PUBLIC wtl)
+endif ()
+
+if (APPLE)
+    _foosdk_glob(SDK_HELPERS_MAC_SOURCES
+            "${_foosdk_glob_root}/foobar2000/helpers-mac/*.m"
+            "${_foosdk_glob_root}/foobar2000/helpers-mac/*.mm"
+    )
+    add_library(foosdk_helpers_mac STATIC ${SDK_HELPERS_MAC_SOURCES})
+    target_include_directories(foosdk_helpers_mac PUBLIC
+            "$<BUILD_INTERFACE:${_foosdk_glob_root}/foobar2000/helpers-mac>"
+            "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000/helpers-mac>"
+    )
+endif ()
+
+_foosdk_glob(PPUI_SOURCES
+        "${_foosdk_glob_root}/libPPUI/*.c"
+        "${_foosdk_glob_root}/libPPUI/*.cpp"
+)
+add_library(foosdk_ppui STATIC ${PPUI_SOURCES})
+target_include_directories(foosdk_ppui PUBLIC
+        "$<BUILD_INTERFACE:${_foosdk_glob_root}/libPPUI>"
+        "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/libPPUI>"
+)
+target_link_libraries(foosdk_ppui PUBLIC pfc wtl)
+
+_foosdk_glob(COMPONENT_CLIENT_SOURCES
+        "${_foosdk_glob_root}/foobar2000/foobar2000_component_client/*.c"
+        "${_foosdk_glob_root}/foobar2000/foobar2000_component_client/*.cpp"
+)
+add_library(foosdk_component_client STATIC ${COMPONENT_CLIENT_SOURCES})
+target_include_directories(foosdk_component_client PUBLIC
+        "$<BUILD_INTERFACE:${_foosdk_glob_root}/foobar2000>"
+        "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000>"
+)
+target_link_libraries(foosdk_component_client PUBLIC pfc)
+
+_foosdk_glob(SAMPLE_SOURCES
+        "${_foosdk_glob_root}/foobar2000/foo_sample/*.c"
+        "${_foosdk_glob_root}/foobar2000/foo_sample/*.cpp"
+)
+_foosdk_glob(SHARED_SOURCES
+        "${_foosdk_glob_root}/foobar2000/shared/*.c"
+        "${_foosdk_glob_root}/foobar2000/shared/*.cpp"
+)
+if (APPLE)
+    _foosdk_glob(SHARED_SOURCES_MACOS
+            "${_foosdk_glob_root}/foobar2000/shared/*.m"
+            "${_foosdk_glob_root}/foobar2000/shared/*.mm"
+    )
+    list(APPEND SHARED_SOURCES ${SHARED_SOURCES_MACOS})
+elseif (WIN32)
+    list(REMOVE_ITEM SHARED_SOURCES "${_foosdk_glob_root}/foobar2000/shared/shared-nix.cpp")
+endif ()
+add_library(foosdk_shared SHARED ${SHARED_SOURCES})
+target_compile_definitions(foosdk_shared PRIVATE SHARED_EXPORTS)
+target_link_libraries(foosdk_shared PRIVATE pfc)
+if (WIN32)
+    target_link_libraries(foosdk_shared PRIVATE DbgHelp Comctl32 UxTheme)
+endif ()
+if (WIN32)
+    _foosdk_glob(SAMPLE_SOURCES_WINDOWS
+            "${_foosdk_glob_root}/foobar2000/foo_sample/*.rc"
+    )
+    list(APPEND SAMPLE_SOURCES ${SAMPLE_SOURCES_WINDOWS})
+elseif (APPLE)
+    _foosdk_glob(SAMPLE_SOURCES_MACOS
+            "${_foosdk_glob_root}/foobar2000/foo_sample/*.m"
+            "${_foosdk_glob_root}/foobar2000/foo_sample/*.mm"
+            "${_foosdk_glob_root}/foobar2000/foo_sample/*.xib")
+    list(APPEND SAMPLE_SOURCES ${SAMPLE_SOURCES_MACOS})
+    list(APPEND SHARED_SOURCES ${SHARED_SOURCES_MACOS})
+endif ()
+target_include_directories(foosdk_shared PUBLIC
+        "$<BUILD_INTERFACE:${_foosdk_glob_root}/foobar2000/shared>"
+        "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000/shared>"
+)
+add_library(foo_sample MODULE ${SAMPLE_SOURCES})
+target_include_directories(foo_sample PUBLIC
+        "$<BUILD_INTERFACE:${_foosdk_glob_root}/foobar2000/foo_sample>"
+        "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000/foo_sample>"
+)
+target_link_libraries(foo_sample PRIVATE foosdk foosdk_helpers foosdk_shared foosdk_component_client)
+if (APPLE)
+    set_target_properties(foo_sample PROPERTIES BUNDLE ON BUNDLE_EXTENSION component)
+    target_link_libraries(foo_sample PRIVATE "-framework Cocoa")
+elseif (WIN32)
+    target_link_libraries(foo_sample PRIVATE foosdk_ppui)
+endif ()
+
+unset(_foosdk_glob_root)
