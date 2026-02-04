cf8b29daf4a1d601c323eb1f1f34765a3a98f979
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2026-02-04 16:03:49 +0100

    Add aliases and rename targets

--- cmake/sources.cmake
+++ cmake/sources.cmake
@@ -16,6 +16,7 @@ target_include_directories(wtl INTERFACE
         "$<BUILD_INTERFACE:${wtl_SOURCE_DIR}/Include>"
         "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/wtl>"
 )
+add_library(foosdk::wtl ALIAS wtl)
 
 set(FLAGS)
 if (WIN32)
@@ -54,6 +55,7 @@ target_include_directories(pfc PUBLIC
         "$<BUILD_INTERFACE:${_foosdk_glob_root}>"
         "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>"
 )
+add_library(foosdk::pfc ALIAS pfc)
 target_compile_definitions(pfc PUBLIC ${FLAGS})
 if (APPLE)
     target_link_libraries(pfc PUBLIC "$<LINK_LIBRARY:FRAMEWORK,Foundation>")
@@ -69,6 +71,7 @@ target_include_directories(foosdk_component_client PUBLIC
         "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000>"
 )
 target_link_libraries(foosdk_component_client PUBLIC pfc)
+add_library(foosdk::component_client ALIAS foosdk_component_client)
 
 _foosdk_glob(SDK_SOURCES
         "${_foosdk_glob_root}/foobar2000/SDK/*.c"
@@ -87,6 +90,7 @@ target_include_directories(foosdk PUBLIC
         "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000>"
 )
 target_link_libraries(foosdk PUBLIC pfc foosdk_component_client)
+add_library(foosdk::foosdk ALIAS foosdk)
 
 _foosdk_glob(SDK_HELPERS_SOURCES
         "${_foosdk_glob_root}/foobar2000/helpers/*.c"
@@ -103,15 +107,16 @@ if (APPLE)
             "${_foosdk_glob_root}/foobar2000/helpers/DarkMode.cpp"
     )
 endif ()
-add_library(foosdk_helpers STATIC ${SDK_HELPERS_SOURCES})
-target_include_directories(foosdk_helpers PUBLIC
+add_library(helpers STATIC ${SDK_HELPERS_SOURCES})
+target_include_directories(helpers PUBLIC
         "$<BUILD_INTERFACE:${_foosdk_glob_root}/foobar2000/helpers>"
         "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000/helpers>"
 )
-target_link_libraries(foosdk_helpers PUBLIC pfc foosdk)
+target_link_libraries(helpers PUBLIC pfc foosdk)
 if (WIN32)
-    target_link_libraries(foosdk_helpers PUBLIC wtl)
+    target_link_libraries(helpers PUBLIC wtl)
 endif ()
+add_library(foosdk::helpers ALIAS helpers)
 
 if (APPLE)
     file(GLOB SDK_HELPERS_MAC_SOURCES CONFIGURE_DEPENDS LIST_DIRECTORIES FALSE RELATIVE "${_foosdk_glob_root}"
@@ -121,16 +126,17 @@ if (APPLE)
     list(SORT SDK_HELPERS_MAC_SOURCES)
     set(SDK_HELPERS_MAC_SOURCES_UNPREFIXED ${SDK_HELPERS_MAC_SOURCES})
     list(TRANSFORM SDK_HELPERS_MAC_SOURCES PREPEND "${_foosdk_glob_root}/")
-    add_library(foosdk_helpers_mac INTERFACE)
-    target_include_directories(foosdk_helpers_mac INTERFACE
+    add_library(helpers_mac INTERFACE)
+    target_include_directories(helpers_mac INTERFACE
             "$<BUILD_INTERFACE:${_foosdk_glob_root}/foobar2000/helpers-mac>"
             "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000/helpers-mac>"
     )
-    target_link_libraries(foosdk_helpers_mac INTERFACE foosdk_helpers)
-    target_sources(foosdk_helpers_mac INTERFACE
+    target_link_libraries(helpers_mac INTERFACE helpers)
+    target_sources(helpers_mac INTERFACE
             "$<BUILD_INTERFACE:${SDK_HELPERS_MAC_SOURCES}>"
             "$<INSTALL_INTERFACE:${SDK_HELPERS_MAC_SOURCES_UNPREFIXED}>"
     )
+    add_library(foosdk::helpers_mac ALIAS helpers_mac)
 endif ()
 
 if (WIN32)
@@ -138,12 +144,13 @@ if (WIN32)
             "${_foosdk_glob_root}/libPPUI/*.c"
             "${_foosdk_glob_root}/libPPUI/*.cpp"
     )
-    add_library(foosdk_ppui STATIC ${PPUI_SOURCES})
-    target_include_directories(foosdk_ppui PUBLIC
+    add_library(ppui STATIC ${PPUI_SOURCES})
+    target_include_directories(ppui PUBLIC
             "$<BUILD_INTERFACE:${_foosdk_glob_root}/libPPUI>"
             "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/libPPUI>"
     )
-    target_link_libraries(foosdk_ppui PUBLIC pfc wtl)
+    target_link_libraries(ppui PUBLIC pfc wtl)
+    add_library(foosdk::ppui ALIAS ppui)
 endif ()
 
 _foosdk_glob(SHARED_SOURCES
@@ -167,29 +174,30 @@ elseif (WIN32)
     list(REMOVE_ITEM SHARED_SOURCES "${_foosdk_glob_root}/foobar2000/shared/shared-nix.cpp")
 endif ()
 if (APPLE)
-    add_library(foosdk_shared SHARED ${SHARED_SOURCES})
-    target_compile_definitions(foosdk_shared PRIVATE SHARED_EXPORTS)
-    target_link_libraries(foosdk_shared PRIVATE pfc)
-    target_link_libraries(foosdk_shared PUBLIC foosdk)
-    target_link_libraries(foosdk_shared PUBLIC "$<LINK_LIBRARY:FRAMEWORK,AppKit>")
-    target_include_directories(foosdk_shared PUBLIC
+    add_library(shared SHARED ${SHARED_SOURCES})
+    target_compile_definitions(shared PRIVATE SHARED_EXPORTS)
+    target_link_libraries(shared PRIVATE pfc)
+    target_link_libraries(shared PUBLIC foosdk)
+    target_link_libraries(shared PUBLIC "$<LINK_LIBRARY:FRAMEWORK,AppKit>")
+    target_include_directories(shared PUBLIC
             "$<BUILD_INTERFACE:${_foosdk_glob_root}/foobar2000/shared>"
             "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000/shared>"
     )
 elseif (WIN32)
-    add_library(foosdk_shared INTERFACE)
+    add_library(shared INTERFACE)
     if (CMAKE_SIZEOF_VOID_P EQUAL 8)
-        set(_foosdk_shared_implib "${_foosdk_glob_root}/foobar2000/shared/shared-x64.lib")
+        set(_shared_implib "${_foosdk_glob_root}/foobar2000/shared/shared-x64.lib")
     else ()
-        set(_foosdk_shared_implib "${_foosdk_glob_root}/foobar2000/shared/shared-Win32.lib")
+        set(_shared_implib "${_foosdk_glob_root}/foobar2000/shared/shared-Win32.lib")
     endif ()
-    target_link_libraries(foosdk_shared INTERFACE "${_foosdk_shared_implib}")
-    target_include_directories(foosdk_shared INTERFACE
+    target_link_libraries(shared INTERFACE "${_shared_implib}")
+    target_include_directories(shared INTERFACE
             "$<BUILD_INTERFACE:${_foosdk_glob_root}/foobar2000/shared>"
             "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/foobar2000/shared>"
     )
-    unset(_foosdk_shared_implib)
+    unset(_shared_implib)
 endif ()
+add_library(foosdk::shared ALIAS shared)
 
 _foosdk_glob(SAMPLE_SOURCES
         "${_foosdk_glob_root}/foobar2000/foo_sample/*.c"
@@ -230,12 +238,12 @@ add_library(foo_sample MODULE ${SAMPLE_SOURCES})
 target_include_directories(foo_sample PRIVATE
         "${_foosdk_glob_root}/foobar2000/foo_sample"
 )
-target_link_libraries(foo_sample PRIVATE foosdk foosdk_helpers foosdk_shared)
+target_link_libraries(foo_sample PRIVATE foosdk helpers shared)
 if (APPLE)
     set_target_properties(foo_sample PROPERTIES BUNDLE ON BUNDLE_EXTENSION component)
     target_link_libraries(foo_sample PRIVATE "$<LINK_LIBRARY:FRAMEWORK,Cocoa>")
 elseif (WIN32)
-    target_link_libraries(foo_sample PRIVATE foosdk_ppui)
+    target_link_libraries(foo_sample PRIVATE ppui)
 endif ()
 
 unset(_foosdk_glob_root)
